4a5aef3065bcbb569b4e9824fc6cb809b3f8dcdc
Onkar <er.onkarjanwa@gmail.com>
2020-01-26 17:39:03 +0530

    Products

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,76 +1,154 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography";
-import styles from '../styles/style.module.css';
 
-class BlogPosts extends React.Component {
+class Work extends React.Component {
   render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title;
-    const siteAuthor = data.site.siteMetadata.author;
-    const posts = data.allMarkdownRemark.edges;
+    const { data } = this.props;
+    console.log(data);
+    const works = [
+        {
+            image: data.rimtim,
+            product: {
+                name: 'RimTim',
+                position: 'Manager Development',
+                desc: 'Responsible for continuous feature releases for RimTim on web app, android app and ios app.',
+                url: 'https://www.rimtim.com'
+            }
+        },
+        {
+            image: data.conx,
+            product: {
+                name: 'Conx',
+                position: 'Manager Development',
+                desc: 'Responsible for continuous feature releases for RimTim on web app, android app and ios app.',
+                url: 'https://www.infocusconx.com'
+            }
+        },
+        {
+            image: data.wegift,
+            product: {
+                name: 'WeGift',
+                position: 'Manager Development',
+                desc: 'Responsible for continuous feature releases for RimTim on web app, android app and ios app.',
+                url: 'https://wegift.io'
+            }
+        },
+        {
+            image: data.reviewkeg,
+            product: {
+                name: 'ReviewKeg',
+                position: 'Manager Development',
+                desc: 'Responsible for continuous feature releases for RimTim on web app, android app and ios app.',
+                url: 'https://www.reviewkeg.com'
+            }
+        },
+        {
+            image: data.hoojook,
+            product: {
+                name: 'Hoojook',
+                position: 'Manager Development',
+                desc: 'Responsible for continuous feature releases for RimTim on web app, android app and ios app.',
+                url: 'http://www.hoojook.com'
+            }
+        },
+        {
+            image: data.rimtimladder,
+            product: {
+                name: 'RimTim Ladder',
+                position: 'Manager Development',
+                desc: 'Responsible for continuous feature releases for RimTim on web app, android app and ios app.',
+                url: 'https://www.rimtimladder.com'
+            }
+        }
+    ];
 
     return (
-      <Layout location={this.props.location} title={siteTitle} author={siteAuthor}>
-        <SEO title="Onkar Janwa" />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <article key={node.fields.slug} className={styles[`article`]}>
-              <header>
-                <h3
-                  style={{
-                    marginBottom: rhythm(1 / 4),
-                  }}
-                >
-                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                    {title}
-                  </Link>
-                </h3>
-                <small>{node.frontmatter.date}</small>
-              </header>
-              <section>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.description || node.excerpt,
-                  }}
-                />
-              </section>
-            </article>
-          )
-        })}
+      <Layout location={this.props.location}>
+        <div className="work__container">
+        <div className="work__container__title"><span>Products</span></div>
+            {works.map((work, index) => {
+                console.log(work);
+            return (
+                <div className="workitem__container" key={index}>
+                    <div>
+                        <Image
+                            fixed={work.image.childImageSharp.fixed}
+                            style={{
+                                marginRight: 0,
+                                marginBottom: 0,
+                                minWidth: 50,
+                                verticalAlign: `middle`,
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <div>{work.product.name}</div>
+                        <div>{work.product.position}</div>
+                        <div>{work.product.desc}</div>
+                        <div>{work.product.url}</div>
+                    </div>
+                </div>
+            )
+            })}
+        </div>
       </Layout>
     )
   }
 }
 
-export default BlogPosts
+export default Work
 
 export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-        author
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
+    query WorkQuery {
+        conx: file(absolutePath: { regex: "/1.png/" }) {
+            childImageSharp {
+                fixed {
+                    ...GatsbyImageSharpFixed
+                }
+            }
+        },
+
+        wegift: file(absolutePath: { regex: "/2.png/" }) {
+            childImageSharp {
+                fixed {
+                    ...GatsbyImageSharpFixed
+                }
+            }
+        },
+
+        rimtim: file(absolutePath: { regex: "/3.jpg/" }) {
+            childImageSharp {
+                fixed {
+                    ...GatsbyImageSharpFixed
+                }
+            }
+        },
+
+        reviewkeg: file(absolutePath: { regex: "/4.png/" }) {
+            childImageSharp {
+                fixed {
+                    ...GatsbyImageSharpFixed
+                }
+            }
+        },
+
+        hoojook: file(absolutePath: { regex: "/5.png/" }) {
+            childImageSharp {
+                fixed {
+                    ...GatsbyImageSharpFixed
+                }
+            }
+        },
+
+        rimtimladder: file(absolutePath: { regex: "/6.png/" }) {
+            childImageSharp {
+                fixed {
+                    ...GatsbyImageSharpFixed
+                }
+            }
         }
-      }
     }
-  }
 `
